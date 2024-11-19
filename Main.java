680b0ffcba38e78912ed3cb49529ff07211ea354
Robert-Alexandru Chitu <62566438+aRobertChitu@users.noreply.github.com>
2024-11-19 23:27:56 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+}
